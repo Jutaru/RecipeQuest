1bc3e5dbe725fa045d8aa688aa73c24d6be35a36
wajdi <wajdi404@gmail.com>
2022-02-09 22:56:29 +0100

    init

---
+++ src/main/java/com/supcom/recipeQuest/RecipeQuestApplication.java
@@ -0,0 +1,13 @@
+package com.supcom.recipeQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supcom/recipeQuest/RecipeQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supcom.recipeQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

dd442d0cd9e2b527953d2549e3afa01baef4fbbb
wajdi <wajdi404@gmail.com>
2022-02-20 18:04:14 +0100

    Login

--- src/main/java/com/supcom/recipeQuest/RecipeQuestApplication.java
+++ src/main/java/com/supcom/recipeQuest/RecipeQuestApplication.java
@@ -2,12 +2,18 @@ package com.supcom.recipeQuest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RecipeQuestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeQuestApplication.class, args);
 	}
-
+	@GetMapping
+	public String hello(){
+		return "Hello world";
+	}
 }
---
+++ src/main/java/com/supcom/recipeQuest/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.supcom.recipeQuest.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
